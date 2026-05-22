9754d7def9294e978c26227bc8bc91dae018e8bd
Vishal Rajput <vishalrajput1034@gmail.com>
2026-05-22 17:47:16 +0530

    migrate sqlite to postgresql

--- app/__init__.py
+++ app/__init__.py
@@ -13,12 +13,21 @@ from app.extensions import db, csrf,migrate
 def create_app(): #factory function which is create flask app
     app =Flask(__name__) #for app start(initialize)
     
-    # app.config['SECRET_KEY'] = 'your-secret-key' # for security 
-    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-key')
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///todo.db') 
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #Tracking off for avoid extra memory use and make peformance fast
+     # ===============================
+    # DATABASE FIX (Render PostgreSQL safe)
+    # ===============================
+    db_url = os.environ.get('DATABASE_URL', 'sqlite:///todo.db')
+
+    # Render fix: postgres:// → postgresql://
+    if db_url.startswith("postgres://"):
+        db_url = db_url.replace("postgres://", "postgresql://", 1)
+    # # app.config['SECRET_KEY'] = 'your-secret-key' # for security 
+    # app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-key')
+    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///todo.db') 
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #Tracking off for avoid extra memory use and make peformance fast
     
-    
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     #Database ko app se connect kara
     db.init_app(app)
     csrf.init_app(app)  # initialize extensions
