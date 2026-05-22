39c4c83c92106aa4b40d02ec6c9a3a77bb90d44f
Vishal Rajput <vishalrajput1034@gmail.com>
2026-05-22 18:04:10 +0530

    Fix Flask SQLAlchemy database configuration and environment variable handling

--- app/__init__.py
+++ app/__init__.py
@@ -17,17 +17,14 @@ def create_app(): #factory function which is create flask app
     # DATABASE FIX (Render PostgreSQL safe)
     # ===============================
     db_url = os.environ.get('DATABASE_URL', 'sqlite:///todo.db')
+    print("DATABASE URL:", db_url)
 
     # Render fix: postgres:// → postgresql://
     if db_url.startswith("postgres://"):
         db_url = db_url.replace("postgres://", "postgresql://", 1)
-    # # app.config['SECRET_KEY'] = 'your-secret-key' # for security 
-    # app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'dev-key')
-    # app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///todo.db') 
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #Tracking off for avoid extra memory use and make peformance fast
-    
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "dev-key")
     #Database ko app se connect kara
     db.init_app(app)
     csrf.init_app(app)  # initialize extensions
