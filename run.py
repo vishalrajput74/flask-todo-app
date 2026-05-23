4dc505e72c6c78e5f0ee9a1332e645f1e22a4e48
Vishal Rajput <vishalrajput1034@gmail.com>
2026-05-23 22:26:52 +0530

    Log active DB URI and task count on startup

--- run.py
+++ run.py
@@ -1,12 +1,15 @@
+from app.models import Task
+
 from app import create_app,db
 import os
 
 app=create_app()  #function call for app creation
-
+print(app.config["SQLALCHEMY_DATABASE_URI"])
 # print("DB Path:", os.path.abspath('instance/todo.db'))
 # print("Current Working Directory:", os.getcwd())
 
 with app.app_context():
+    print(Task.query.count())
     db.create_all()
     
 if __name__=="__main__":
