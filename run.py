54cef6ee305da3c7b32adf6243028ee28ff27647
Vishal Rajput <vishalrajput1034@gmail.com>
2026-05-26 12:07:59 +0530

    remove debug print statement

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ from app import create_app,db
 import os
 
 app=create_app()  #function call for app creation
-print(app.config["SQLALCHEMY_DATABASE_URI"])
+# print(app.config["SQLALCHEMY_DATABASE_URI"])
 # print("DB Path:", os.path.abspath('instance/todo.db'))
 # print("Current Working Directory:", os.getcwd())
 
